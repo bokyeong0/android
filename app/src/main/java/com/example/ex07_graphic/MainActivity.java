d2bbb213e656df137747b232f490447870009fe6
bokyeong0 <maserpiece.bk@gmail.com>
2019-04-05 14:25:51 +0900

    init

--- app/src/main/java/com/example/ex07_graphic/MainActivity.java
+++ app/src/main/java/com/example/ex07_graphic/MainActivity.java
@@ -13,7 +13,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
-
+/*git*/
     public void onClick(View v){
         Intent intent = null;
 
