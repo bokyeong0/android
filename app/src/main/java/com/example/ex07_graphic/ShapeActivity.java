5ef6b73284000ce790df6d4651270d81d535090f
bokyeong0 <maserpiece.bk@gmail.com>
2019-04-05 14:19:24 +0900

    init

---
+++ app/src/main/java/com/example/ex07_graphic/ShapeActivity.java
@@ -0,0 +1,12 @@
+package com.example.ex07_graphic;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ShapeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
